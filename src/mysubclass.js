c6253125421ab8eeb48ff444ae802491e191cd4a
thebjorn <bp@norsktest.no>
2020-04-12 00:25:09 +0200

    inherit from imported module.

--- src/mysubclass.js
+++ src/mysubclass.js
@@ -1,23 +1,11 @@
 
-// const bpjslib = require('bpjslib');
-// import * as bpjslib from 'bpjslib';
 import bpjslib from 'bpjslib';
 
 
-// export class MySubClass extends bpjslib.MyClass {
-//     constructor(...args) {
-//         super({
-//             myval: 1
-//         }, ...args);
-//     }
-// }
-
-
-export class MySubClass {
+export class MySubClass extends bpjslib.MyClass {
     constructor(...args) {
-        console.log("MYCLASS:", bpjslib);
-        console.log("MYCLASS:", bpjslib.MyClass);
-        const myclass = new bpjslib.MyClass();
-        this.myval = myclass.myval - 41;
+        super({
+            myval: 1
+        }, ...args);
     }
 }
